82203d1ae62e0c03b410f33a4177c87fb10bc79f
jose-rizal <jorizivannvillanueva14@gmail.com>
2020-11-08 17:14:26 +0800

    Test file

---
+++ number_game.py
@@ -0,0 +1,18 @@
+# Number game
+#  Rules: Guest the Random number given by the operator. The number is
+# 0>number>=20.
+import random
+n = 20
+to_be_guessed = int(n * random.random()) + 1
+guess = 0
+while guess != to_be_guessed:
+	guess = int(input("new nunmber:"))
+	if guess > 0:
+		if guess > to_be_guessed:
+			print("Number too large")
+		elif guess < to_be_guessed:
+			print("Number too small")
+	else:
+		print("Sorry that you're giving up!")
+else:
+	print("Congration. You made it!")
