ecbdc145c2f042e7e5f5bc94455d9f417d60f9d9
j0wiz <jorizivannvillanueva18@gmail.com>
2021-02-05 12:40:24 +0800

    Fix typo

--- number_game.py
+++ number_game.py
@@ -8,7 +8,7 @@ n = 20
 to_be_guessed = int(n * random.random()) + 1
 guess = 0
 while guess != to_be_guessed:
-	guess = int(input("new nunmber:"))
+	guess = int(input("new number:"))
 	if guess > 0:
 		if guess > to_be_guessed:
 			print("Number too large")
